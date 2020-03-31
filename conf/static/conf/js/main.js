f749d988870cd7d65d482996294915d099f8b754
pincoin <pincoins@gmail.com>
2020-04-01 02:07:01 +0700

    - mobile search box

--- conf/static/conf/js/main.js
+++ conf/static/conf/js/main.js
@@ -10,14 +10,26 @@ document.addEventListener('DOMContentLoaded', function () {
     var instances = M.Sidenav.init(elems, {'edge': 'right'});
 });
 
-$('document').ready(function() {
-   $('#navbar-search1').on('click', function() {
-       $('#navbar-top-right').hide();
-       $('#navbar-search-box1').show();
-   });
+$('document').ready(function () {
+    $('#navbar-search1').on('click', function () {
+        $('#navbar-top-right').hide();
+        $('#navbar-search-box1').show();
+    });
 
-   $('#navbar-search-close1').on('click', function() {
-       $('#navbar-top-right').show();
-       $('#navbar-search-box1').hide();
-   });
+    $('#navbar-search-close1').on('click', function () {
+        $('#navbar-top-right').show();
+        $('#navbar-search-box1').hide();
+    });
+
+    $('#navbar-search2').on('click', function () {
+        $(this).hide();
+        $('.brand-logo').hide();
+        $('#navbar-search-box2').show();
+    });
+
+    $('#navbar-search-close2').on('click', function () {
+        $('#navbar-search2').show();
+        $('.brand-logo').show();
+        $('#navbar-search-box2').hide();
+    });
 });
