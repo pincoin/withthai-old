bb4b5ad31efe100b57df4a897625792d1547fba2
pincoin <pincoins@gmail.com>
2020-04-02 21:59:12 +0700

    - submenu scroll

--- conf/static/conf/js/main.js
+++ conf/static/conf/js/main.js
@@ -34,3 +34,12 @@ $('document').ready(function () {
     });
 
 });
+
+$(window).on('load resize', function () {
+    if ($(this).width() < 992) {
+        var h = $(window).height();
+
+        // 38x7=266, 38x8=304
+        $('.submenu-scroll').css({'max-height': h - 228, 'min-height': h - 228});
+    }
+});
