aa645fa7a7ef9bf41ed5e424c14396d12085050e
pincoin <pincoins@gmail.com>
2020-04-04 22:07:11 +0700

    - social auth

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -30,6 +30,10 @@ INSTALLED_APPS += [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.kakao',
+    'allauth.socialaccount.providers.naver',
 ]
 
 MIDDLEWARE = [
@@ -80,6 +84,25 @@ AUTHENTICATION_BACKENDS = (
 )
 
 SITE_ID = 1
+
+# django.contrib.auth settings for allauth
+PASSWORD_RESET_TIMEOUT_DAYS = 1  # default=3
+LOGIN_URL = '/accounts/login/'  # default=/accounts/login/
+LOGOUT_URL = '/accounts/logout/'  # default=/accounts/logout/
+LOGIN_REDIRECT_URL = '/shop/default'  # default=/accounts/profile/
+# LOGOUT_REDIRECT_URL = '/'
+
+# django-allauth
+# DEFAULT_FROM_EMAIL = secret.EMAIL_NO_REPLY
+# ACCOUNT_ADAPTER = 'member.adapters.MyAccountAdapter'
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 5
+ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 300
+# ACCOUNT_SIGNUP_FORM_CLASS = 'member.forms.MemberSignupForm'
+ACCOUNT_LOGOUT_ON_PASSWORD_CHANGE = True  # default=False
+SOCIALACCOUNT_AUTO_SIGNUP = False
 
 SOCIALACCOUNT_PROVIDERS = {
     'facebook': {},
