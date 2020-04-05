a4fe3b457c1ae92e5262807187c9d9727c6310e2
pincoin <pincoins@gmail.com>
2020-04-05 08:41:29 +0700

    - allauth templates

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -59,6 +59,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'conf', 'templates'),
+            os.path.join(BASE_DIR, 'allauth', 'templates'),
+            os.path.join(BASE_DIR, 'blog', 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
