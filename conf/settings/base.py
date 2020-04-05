9fa43a79156ec8119cda34aa95627a243f321bd2
pincoin <pincoins@gmail.com>
2020-04-05 21:29:51 +0700

    - magazine app

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -39,7 +39,7 @@ INSTALLED_APPS += [
 
 INSTALLED_APPS += [
     'member',
-    'blog',
+    'magazine',
 ]
 
 MIDDLEWARE = [
--- magazine/apps.py
+++ magazine/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
-class MemberConfig(AppConfig):
-    name = 'member'
-    verbose_name = _('Member')
+
+class MagazineConfig(AppConfig):
+    name = 'magazine'
+    verbose_name = _('Magazine')
