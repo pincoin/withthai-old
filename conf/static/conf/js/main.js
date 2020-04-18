c420745dabfd27d00c8b2591eef1f50db677d545
pincoin <pincoins@gmail.com>
2020-04-19 03:20:26 +0900

    - scroll expand

--- conf/static/conf/js/main.js
+++ conf/static/conf/js/main.js
@@ -1,43 +1,8 @@
-/* left side bar for mobile */
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('#mobile-menu');
-    var instances = M.Sidenav.init(elems, {});
-});
-
-/* right side bar for mobile */
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('#mobile-cart');
-    var instances = M.Sidenav.init(elems, {'edge': 'right'});
-});
-
 $('document').ready(function () {
-    $('#navbar-search1').on('click', function () {
-        $('#navbar-top-right').hide();
-        $('#navbar-search-box1').show();
-    });
-
-    $('#navbar-search-close1').on('click', function () {
-        $('#navbar-top-right').show();
-        $('#navbar-search-box1').hide();
-    });
-
-    $('#navbar-search2').on('click', function () {
-        $(this).hide();
-        $('.brand-logo').hide();
-        $('#navbar-search-box2').show();
-    });
-
-    $('#navbar-search-close2').on('click', function () {
-        $('#navbar-search2').show();
-        $('.brand-logo').show();
-        $('#navbar-search-box2').hide();
-    });
-
-    $('.parallax').parallax();
 });
 
 $(window).on('load resize', function () {
-    if ($(this).width() < 992) {
+    if ($(this).width() < 960) {
         var h = $(window).height();
 
         // 38x7=266, 38x8=304
