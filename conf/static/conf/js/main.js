9ee8d9e87f46de8bfef2943005ff242935a35214
pincoin <pincoins@gmail.com>
2020-05-17 05:04:18 +0900

    - navbar box shadow

--- conf/static/conf/js/main.js
+++ conf/static/conf/js/main.js
@@ -11,4 +11,13 @@ $(document).ready(function () {
     $('#language-selector').on('change', function (e) {
         this.form.submit();
     });
+
+    $(window).on("scroll", function () {
+        var wn = $(window).scrollTop();
+        if (wn > 20) {
+            $(".navbar").css("box-shadow", "0 10px 5px -2px rgba(0,0,0,0.2)").css("transition-duration", "0.1s");
+        } else {
+            $(".navbar").css("box-shadow", "none");
+        }
+    });
 });
