1f28337f196f825de501e7f0a2dfea0ddf893ced
pincoin <pincoins@gmail.com>
2020-05-25 00:10:16 +0900

    - club link from admin

--- golf/admin.py
+++ golf/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 
 from . import models
@@ -37,7 +39,16 @@ class ClubAdmin(admin.ModelAdmin):
                     'slug', 'hole', 'green_fee_selling_price', 'position')
     list_filter = ('district__province__title_english', 'hole')
     prepopulated_fields = {'slug': ('title_english',)}
+    readonly_fields = ('club_link',)
     ordering = ('district', 'position',)
+
+    def club_link(self, obj=None):
+        return mark_safe('<a href="{url}">{text}</a>'.format(
+            url=reverse('golf:golf-club-booking', args=(obj.slug,)),
+            text='{}'.format(obj.title_english),
+        ))
+
+    club_link.short_description = _('Golf club')
 
 
 class RateAdmin(admin.ModelAdmin):
