a0fb672074cd222dfb3a2fa48b563a907770dda2
pincoin <pincoins@gmail.com>
2020-05-31 05:00:35 +0900

    - sign up test

--- member/urls.py
+++ member/urls.py
@@ -1,4 +1,10 @@
+from django.urls import path
+
+from . import views
+
 app_name = 'member'
 
 urlpatterns = [
+    path('social-signup-test',
+         views.SocialSignUpTestView.as_view(), name='social-sign-up-test-view'),
 ]
