c9adb3ce8506d9debdc2acf35801b2a3311b5efa
pincoin <pincoins@gmail.com>
2020-05-30 21:01:37 +0900

    - login log signal

--- member/apps.py
+++ member/apps.py
@@ -5,3 +5,6 @@ from django.utils.translation import gettext_lazy as _
 class MemberConfig(AppConfig):
     name = 'member'
     verbose_name = _('Member')
+
+    def ready(self):
+        import member.signals
--- member/models.py
+++ member/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+
+class LoginLog(TimeStampedModel):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_('User'),
+        null=True,
+        blank=True,
+        editable=True,
+        on_delete=models.SET_NULL,
+    )
+
+    ip_address = models.GenericIPAddressField(
+        verbose_name=_('IP address'),
+    )
+
+    class Meta:
+        verbose_name = _('Login log')
+        verbose_name_plural = _('Login logs')
+
+    def __str__(self):
+        return '{} {} {}'.format(self.user.email, self.ip_address, self.created)
