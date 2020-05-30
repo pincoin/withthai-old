cf9d34112cf94817b0c72e14d6cb9249b693e24d
pincoin <pincoins@gmail.com>
2020-05-30 22:29:11 +0900

    - sign up

---
+++ member/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+
+class MemberSignupForm(forms.Form):
+    first_name = forms.CharField(
+        label=_('First name'),
+        max_length=30,
+        help_text=_('Written in English'),
+        widget=forms.TextInput(),
+    )
+
+    last_name = forms.CharField(
+        label=_('Last name'),
+        max_length=30,
+        help_text=_('Written in English'),
+        widget=forms.TextInput(),
+    )
+
+    phone = forms.RegexField(
+        label=_('Phone number'),
+        widget=forms.TextInput(),
+        regex=r'^\+?1?\d{9,15}$',
+        error_messages={
+            'invalid': _('Invalid phone number format'),
+        }
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(MemberSignupForm, self).__init__(*args, **kwargs)
+
+    def signup(self, request, user):
+        # Required fields for Django default model
+        user.first_name = self.cleaned_data['first_name'].strip()
+        user.last_name = self.cleaned_data['last_name'].strip()
+
+        # TODO: Profile and Groups
+
+        user.save()
