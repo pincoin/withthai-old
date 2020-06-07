6fcc20f50f4e10921c4c568909d0886f8d9d93ca
pincoin <pincoins@gmail.com>
2020-06-08 01:21:41 +0900

    - profile

--- member/urls.py
+++ member/urls.py
@@ -5,6 +5,9 @@ from . import views
 app_name = 'member'
 
 urlpatterns = [
+    path('profile/',
+         views.SocialSignUpTestView.as_view(), name='profile'),
+
     path('social-signup-test',
          views.SocialSignUpTestView.as_view(), name='social-sign-up-test-view'),
 ]
