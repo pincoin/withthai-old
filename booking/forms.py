f0e53d2da9c8e50f1624b2ed4e31d60d21be5315
pincoin <pincoins@gmail.com>
2020-06-10 00:15:10 +0900

    - date / time validation

--- booking/forms.py
+++ booking/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from . import models
@@ -16,6 +17,8 @@ class GolfClubBookingForm(forms.ModelForm):
         self.club = kwargs.pop('club', None)
         self.rates = kwargs.pop('rates', None)
 
+        self.now = timezone.datetime.now()
+
         super(GolfClubBookingForm, self).__init__(*args, **kwargs)
 
         self.fields['pax'].choices = tuple((str(i), str(i)) for i in [x for x in range(1, int(self.club.max_pax) + 1)])
@@ -27,14 +30,29 @@ class GolfClubBookingForm(forms.ModelForm):
     def clean_round_date(self):
         data = self.cleaned_data['round_date']
 
-        # raise forms.ValidationError(_('Invalid round date'))
+        if (self.now + timezone.timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0) > \
+                self.now > \
+                self.now.replace(hour=16, minute=0, second=0, microsecond=0):
+            if data < \
+                    timezone.datetime.date(self.now + timezone.timedelta(self.club.weekdays_min_in_advance + 1)) \
+                    or data > \
+                    timezone.datetime.date(self.now + timezone.timedelta(self.club.weekdays_max_in_advance + 1)):
+                raise forms.ValidationError(_('Invalid round date'))
+        else:
+            if data < \
+                    timezone.datetime.date(self.now + timezone.timedelta(self.club.weekdays_min_in_advance)) \
+                    or data > \
+                    timezone.datetime.date(self.now + timezone.timedelta(self.club.weekdays_max_in_advance)):
+                raise forms.ValidationError(_('Invalid round date'))
 
         return data
 
     def clean_round_time(self):
         data = self.cleaned_data['round_time']
 
-        # raise forms.ValidationError(_('Invalid round time'))
+        if data < timezone.datetime.strptime('06:00', '%H:%M').time() \
+                or data > timezone.datetime.strptime('10:30', '%H:%M').time():
+            raise forms.ValidationError(_('Invalid round time'))
 
         return data
 
@@ -49,8 +67,11 @@ class GolfClubBookingForm(forms.ModelForm):
     def clean(self):
         super().clean()
 
+        # 1. Authentication and Group membership
         if not self.request.user.is_authenticated or not self.request.user.groups.filter(name='customers').exists():
             raise forms.ValidationError(_('You have to sign in as a customer for booking.'))
+
+        # 2. Green fee validation
 
 
 class SearchForm(forms.Form):
