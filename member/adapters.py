d0803146af325d6a7ffafcdb70ef7b69ea534a9f
jonghwa <pincoins@gmail.com>
2020-08-02 02:37:03 +0900

    no local signup

--- member/adapters.py
+++ member/adapters.py
@@ -1,9 +1,14 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
+from django.urls import reverse
+
 
 class MyAccountAdapter(DefaultAccountAdapter):
-    pass
+    def is_open_for_signup(self, request):
+        if request.path.rstrip('/') == reverse('account_signup').rstrip('/'):
+            return False
+        return True
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
