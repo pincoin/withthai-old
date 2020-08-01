04d23fd9f97217094464769b9e520ec0f1e7eb30
jonghwa <pincoins@gmail.com>
2020-08-02 03:44:15 +0900

    social account sign up forms

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -134,7 +134,6 @@ ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 5
 ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 300
 # ACCOUNT_SIGNUP_FORM_CLASS = 'member.forms.MemberSignupForm'
 ACCOUNT_FORMS = {
-    'signup': 'member.forms.MemberSignupForm',
     'change_password': 'member.forms.MemberChangePasswordForm',
     'set_password': 'member.forms.MemberSetPasswordForm',
     'reset_password': 'member.forms.MemberResetPasswordForm',
@@ -144,6 +143,9 @@ ACCOUNT_EMAIL_SUBJECT_PREFIX = _('[WITH THAI] ')
 ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = False
 SOCIALACCOUNT_AUTO_SIGNUP = False
 SOCIALACCOUNT_ADAPTER = 'member.adapters.MySocialAccountAdapter'
+SOCIALACCOUNT_FORMS = {
+    'signup': 'member.forms.MemberSignupForm',
+}
 
 SOCIALACCOUNT_PROVIDERS = {
     'facebook': {},
