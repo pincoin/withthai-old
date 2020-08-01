029408afcd4bf39df5e81b66aa61c821046aea72
jonghwa <pincoins@gmail.com>
2020-08-02 02:20:08 +0900

    sign up test

--- member/adapters.py
+++ member/adapters.py
@@ -1,16 +1,10 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from django.core.exceptions import ValidationError
 
 
 class MyAccountAdapter(DefaultAccountAdapter):
-    def is_open_for_signup(self, request):
-        return False
+    pass
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
-    def is_open_for_signup(self, request, sociallogin):
-        return True
-
-    def validate_disconnect(self, account, accounts):
-        raise ValidationError('Cannot disconnect')
+    pass
