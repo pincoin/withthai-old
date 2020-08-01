0808b9439419d1c76c28703cf9c0a8d0a90d3157
jonghwa <pincoins@gmail.com>
2020-08-02 03:41:49 +0900

    comments

--- member/adapters.py
+++ member/adapters.py
@@ -8,6 +8,7 @@ from django.utils.translation import gettext_lazy as _
 
 class MyAccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request):
+        # Prevent local sign up
         if request.path.rstrip('/') == reverse('account_signup').rstrip('/'):
             return False
         return True
@@ -15,4 +16,5 @@ class MyAccountAdapter(DefaultAccountAdapter):
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
     def validate_disconnect(self, account, accounts):
+        # Prevent disconnection
         raise ValidationError(_('You cannot disconnect social media account.'))
